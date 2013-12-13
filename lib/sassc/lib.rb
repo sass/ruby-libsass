83edd0c4daaf7d7d29775ab1904910ed246455b0
leaf corcoran <leafot@gmail.com>
2013-12-13 13:45:55 -0800

    just assume the package is installed into the system

--- lib/sassc/lib.rb
+++ lib/sassc/lib.rb
@@ -6,7 +6,7 @@ module SassC
   # Represents the exact wrapper around libsass
   module Lib
     extend FFI::Library
-    ffi_lib File.join(File.dirname(__FILE__), '../libsass.bundle')
+    ffi_lib "sass"
     attach_function :sass_new_context, [], :pointer
     attach_function :sass_new_file_context, [], :pointer
     attach_function :sass_new_folder_context, [], :pointer
